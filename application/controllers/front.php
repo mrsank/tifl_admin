8158528f6f6dc97117f74addc410e73ea5e70844
Harisankar <mrsank@gmail.com>
2015-05-14 15:50:50 +0530

    Functions added for basic functions

--- application/controllers/front.php
+++ application/controllers/front.php
@@ -1,27 +1,63 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Front extends CI_Controller {
 
 	/**
-	 * Index Page for this controller.
 	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
 	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 * Controller for front end files
+	 *
+	 *
 	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
+	
+	public function index(){
+		$data['panel'] = "index";
+		$data['base'] = base_url();
+		$this->load->view('front/header',$data);
+		$this->load->view('front/index');
+		$this->load->view('front/footer');
+	}
+	
+	public function aboutus(){
+		$data['panel'] = "about";
+		$data['base'] = base_url();
+		$this->load->view('front/header',$data);
+		$this->load->view('front/about');
+		$this->load->view('front/footer');
+	}
+	
+	public function achievements(){
+		$data['panel'] = "achieve";
+		$data['base'] = base_url();
+		$this->load->view('front/header',$data);
+		$this->load->view('front/achievements');
+		$this->load->view('front/footer');
+	}
+	
+	public function germany(){
+		$data['panel'] = "germany";
+		$data['base'] = base_url();
+		$this->load->view('front/header',$data);
+		$this->load->view('front/germany');
+		$this->load->view('front/footer');
+	}
+	
+	public function gruppe(){
+		$data['panel'] = "gruppe";
+		$data['base'] = base_url();
+		$this->load->view('front/header',$data);
+		$this->load->view('front/gruppe');
+		$this->load->view('front/footer');
+	}
+	
+	public function contactus(){
+		$data['panel'] = "contact";
+		$data['base'] = base_url();
+		$this->load->view('front/header',$data);
+		$this->load->view('front/contact');
+		$this->load->view('front/footer');
 	}
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+/* End of file front.php */
+/* Location: ./application/controllers/front.php */
